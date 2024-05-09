f182e41abfa416996e3a64ff9812aa2e59302865
Vanessa Bueno <vanessa.webdeveloper@gmail.com>
2024-05-09 15:53:52 -0300

    Exemplos de Tipos de Variáveis

---
+++ java-variaveis/src/TiposDeVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposDeVariaveis {
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
+
+        String meuNome = "Vanessa Bueno";
+    }
+}
