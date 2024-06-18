a2859b72f46e701eefe2bcaab3e40ff03f9ced8a
Vanessa Bueno <vanessa.webdeveloper@gmail.com>
2024-06-18 15:56:45 -0300

    Exemplo tratamento de erros com Try-Catch

--- terminal-e-argumentos-scanner/src/AboutMe.java
+++ terminal-e-argumentos-scanner/src/AboutMe.java
@@ -1,27 +1,34 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) throws Exception {
-        // criando objeto scanner
-        @SuppressWarnings("resource")
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        try {
+            // criando objeto scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("\n Digite seu nome");
-        String nome = scanner.next();
+            System.out.println("\n Digite seu nome");
+            String nome = scanner.next();
 
-        System.out.println("\n Digite seu sobrenome");
-        String sobrenome = scanner.next();
+            System.out.println("\n Digite seu sobrenome");
+            String sobrenome = scanner.next();
 
-        System.out.println("\n Digite sua idade");
-        int idade = scanner.nextInt();
+            System.out.println("\n Digite sua idade");
+            int idade = scanner.nextInt();
 
-        System.out.println("\n Digite sua altura");
-        double altura = scanner.nextDouble();
+            System.out.println("\n Digite sua altura");
+            double altura = scanner.nextDouble();
 
-        // imprimindo os dados obtidos pelo usuário
-        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos");
-        System.out.println("Minha altura é " + altura + "cm");
+            // imprimindo os dados obtidos pelo usuário
+            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + " anos");
+            System.out.println("Minha altura é " + altura + "cm");
+
+            scanner.close();
+        }
+        catch (InputMismatchException exception) {
+            System.err.println("Os campos idade e altura precisam ser numéricos!");
+        }        
     }
 }
