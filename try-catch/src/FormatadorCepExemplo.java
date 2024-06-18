71c6328ae5ee80db03a147ec093299256503f4cb
Vanessa Bueno <vanessa.webdeveloper@gmail.com>
2024-06-18 16:28:27 -0300

    Tratamento de Exceções

---
+++ try-catch/src/FormatadorCepExemplo.java
@@ -0,0 +1,18 @@
+public class FormatadorCepExemplo {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("2376506");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoException e) {
+            e.printStackTrace();
+            System.err.println("Cep inválido");
+        }
+    }
+
+    static String formatarCep(String cep) throws CepInvalidoException {
+        if (cep.length() != 8) 
+            throw new CepInvalidoException();
+
+        return "23.765-064";        
+    }
+}
